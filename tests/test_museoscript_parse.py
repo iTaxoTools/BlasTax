fbc9bf909bce02c99e304a632c54abc4ba2eae71
Nikita Kulikov <149519830+KulyaNikita@users.noreply.github.com>
2024-08-02 13:32:20 +0200

    Add files via upload
    Tests for regular blast and museoscript added

---
+++ tests/test_museoscript_parse.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Literal, NamedTuple
+
+import pytest
+
+from core import museoscript_parse
+
+TEST_DATA_DIR = Path(__file__).parent / Path(__file__).stem
+
+class MuseoTest(NamedTuple):
+    blast_path: str
+    output_path:  str
+    pident_threshold: float
+
+    def validate(self, tmp_path: Path) -> None:
+       blast_path = TEST_DATA_DIR / self.blast_path
+       output_path = tmp_path / self.output_path
+       print(f"Blast Path: {blast_path}")
+       print(f"Output Path: {output_path}")
+       museoscript_parse(
+            str(blast_path),
+            str(output_path),
+            self.pident_threshold,
+        )
+
+       assert output_path.exists()
+
+# New blast tests
+museo_tests = [
+    MuseoTest(
+        "blast_output.out",
+        "museoscript_output.out",
+        0.9,
+    ),
+]
+
+@pytest.mark.parametrize("test", museo_tests)
+def test_museoscript(test: MuseoTest, tmp_path: Path) -> None:
+    test.validate(TEST_DATA_DIR)
---
+++ tests/test_regular_blast.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Literal, NamedTuple
+
+import pytest
+
+from core import run_blast
+
+TEST_DATA_DIR = Path(__file__).parent / Path(__file__).stem
+#TEST_DATA_DIR = Path(__file__).parent / "test_data"
+
+class BlastTest(NamedTuple):
+    blast_binary: str
+    query_path: str
+    database_path: str
+    output_path: str
+    evalue: str
+    num_threads: int
+    outfmt: str
+    other: str
+
+    def validate(self, tmp_path: Path) -> None:
+        query_path = TEST_DATA_DIR / self.query_path
+        database_path = TEST_DATA_DIR / self.database_path
+        output_path = tmp_path / self.output_path
+        assert run_blast(
+            self.blast_binary,
+            str(query_path),
+            str(database_path),
+            str(output_path),
+            self.evalue,
+            self.num_threads,
+            self.outfmt,
+            self.other,
+        )
+        assert output_path.exists()
+
+# New blast tests
+blast_tests = [
+    BlastTest(
+        "blastn",
+        "malamini.fas",
+        "mala.fas",
+        "blast_output.txt",
+        "0.001",
+        1,
+        "1",
+        "",
+    ),
+]
+
+@pytest.mark.parametrize("test", blast_tests)
+def test_run_blast(test: BlastTest, tmp_path: Path) -> None:
+    output_dir = TEST_DATA_DIR / "blast_output"
+#    output_dir.mkdir(exist_ok=True)
+    test.validate(TEST_DATA_DIR)
+#    test.validate(tmp_path)
