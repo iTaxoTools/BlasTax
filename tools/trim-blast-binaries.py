6293e26706e16cbf88277a4d6858c9ef8fdf62f1
StefanPatman <stefanpatman91@gmail.com>
2024-08-22 19:08:54 +0300

    Force bundling for windows amd macos

--- tools/trim-blast-binaries.py
+++ tools/trim-blast-binaries.py
@@ -19,12 +19,15 @@ KEPT_SUFFIXES = [
 
 
 def main():
+    count = 0
     for binary in BIN.iterdir():
         if binary.stem in KEPT_BINARIES:
             continue
         if binary.suffix in KEPT_SUFFIXES:
             continue
         binary.unlink()
+        count += 1
+    print(f"Trimmed {count} binaries!")
 
 
 if __name__ == "__main__":
