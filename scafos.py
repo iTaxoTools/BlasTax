4d206fa8e5a6a483e83a66bd80b919e42cd14cc0
StefanPatman <stefanpatman91@gmail.com>
2024-10-15 16:00:42 +0300

    Add scafos mean fuse

--- scafos.py
+++ scafos.py
@@ -1,9 +1,14 @@
 from __future__ import annotations
 
-from collections import Counter
+from collections import Counter, defaultdict
 from enum import Enum, auto
-from typing import Iterator
+from itertools import groupby
+from pathlib import Path
+from typing import Iterator, NamedTuple
 
+from itaxotools.taxi2.distances import Distance, DistanceHandler, DistanceMetric
+from itaxotools.taxi2.handlers import FileHandler
+from itaxotools.taxi2.pairs import SequencePair, SequencePairs
 from itaxotools.taxi2.sequences import Sequence, Sequences
 
 
@@ -11,6 +16,26 @@ class TagMethod(Enum):
     SpeciesAfterPipe = auto()
     SpeciesBeforeFirstUnderscore = auto()
     SpeciesBeforeSecondUnderscore = auto()
+
+
+class DistanceGroups(NamedTuple):
+    key: str
+    others: list[Distance]
+
+
+class AggregatedDistances(NamedTuple):
+    sequence: Sequence
+    species_distances: dict[str, list[float]]
+
+
+class AggregatedMean(NamedTuple):
+    sequence: Sequence
+    mean: float
+
+
+class MeanGroups(NamedTuple):
+    key: str
+    items: list[AggregatedMean]
 
 
 def _tag_species_after_pipe(sequence: Sequence) -> Sequence:
@@ -53,7 +78,7 @@ def count_non_gaps(seq: str) -> int:
     return counter.total() - gaps
 
 
-def fuse_by_max_length(sequences: Iterator[Sequence]) -> Sequences:
+def fuse_by_max_length(sequences: Sequences) -> Sequences:
     species_dict: dict[str, Sequence] = {}
     species_length: dict[str, Sequence] = {}
     for sequence in sequences:
@@ -67,3 +92,88 @@ def fuse_by_max_length(sequences: Iterator[Sequence]) -> Sequences:
             species_dict[species] = sequence
             species_length[species] = length
     return Sequences(list(species_dict.values()))
+
+
+def drop_identical_pairs(pairs: Iterator[SequencePair]) -> Iterator[SequencePair]:
+    for pair in pairs:
+        if pair.x.id != pair.y.id:
+            yield pair
+
+
+def calculate_distances(pairs: Iterator[SequencePair]) -> Iterator[Distance]:
+    metric = DistanceMetric.Uncorrected()
+    for x, y in pairs:
+        yield metric.calculate(x, y)
+
+
+def report_distances(distances: Iterator[Distance], path: Path) -> Iterator[Distance]:
+    with DistanceHandler.Linear.WithExtras(path, "w") as file:
+        for distance in distances:
+            file.write(distance)
+            yield distance
+
+
+def group_distances(distances: Iterator[Distance]) -> Iterator[DistanceGroups]:
+    keyfunc = lambda distance: distance.x.id
+    for left, group in groupby(distances, keyfunc):
+        yield DistanceGroups(left, list(group))
+
+
+def aggregate_groups(groups: Iterator[DistanceGroups]) -> Iterator[AggregatedDistances]:
+    for group in groups:
+        distance_dict: dict[str, list[float]] = defaultdict(list)
+        for distance in group.others:
+            if distance.d is None:
+                continue
+            distance_dict[distance.y.extras["species"]].append(distance.d)
+        sequence = group.others[0].x
+        assert sequence.id == group.key
+        yield AggregatedDistances(sequence, distance_dict)
+
+
+def calculate_mean(data: Iterator[AggregatedDistances]) -> Iterator[AggregatedMean]:
+    for item in data:
+        species = item.sequence.extras["species"]
+        if species in item.species_distances:
+            del item.species_distances[species]
+        means: list[float] = []
+        for distances in item.species_distances.values():
+            mean = sum(distances) / len(distances)
+            means.append(mean)
+        mean_of_means = sum(means) / len(means)
+        yield AggregatedMean(item.sequence, mean_of_means)
+
+
+def report_means(means: Iterator[AggregatedMean], path: Path) -> Iterator[AggregatedMean]:
+    with FileHandler.Tabular.Tabfile(path, "w", columns=["id", "species", "mean"]) as file:
+        for item in means:
+            file.write((item.sequence.id, item.sequence.extras["species"], str(item.mean)))
+            yield item
+
+
+def group_species(data: Iterator[AggregatedMean]) -> Iterator[DistanceGroups]:
+    keyfunc = lambda item: item.sequence.extras["species"]
+    data = sorted(data, key=keyfunc)
+    for key, group in groupby(data, keyfunc):
+        yield MeanGroups(key, list(group))
+
+
+def keep_minimum_mean(groups: Iterator[MeanGroups]) -> Iterator[Sequence]:
+    for group in groups:
+        selected = min(group.items, key=lambda item: item.mean)
+        yield selected.sequence
+
+
+def fuse_by_min_distance(sequences: Sequences) -> Sequences:
+    pairs = SequencePairs.fromProduct(sequences, sequences)
+    pairs = drop_identical_pairs(pairs)
+    distances = calculate_distances(pairs)
+    distances = report_distances(distances, "report_distances.txt")
+    groups = group_distances(distances)
+    aggregated = aggregate_groups(groups)
+    means = calculate_mean(aggregated)
+    means = report_means(means, "report_means.txt")
+    groups = group_species(means)
+    sequences = keep_minimum_mean(groups)
+
+    return Sequences(list(sequences))
--- tests/test_scafos.py
+++ tests/test_scafos.py
@@ -3,7 +3,7 @@ from typing import NamedTuple
 import pytest
 
 from itaxotools.taxi2.sequences import Sequence, Sequences
-from scafos import TagMethod, count_non_gaps, fuse_by_max_length, tag_species_by_method
+from scafos import TagMethod, count_non_gaps, fuse_by_max_length, fuse_by_min_distance, tag_species_by_method
 
 
 class TagTest(NamedTuple):
@@ -38,6 +38,18 @@ class FuseTest(NamedTuple):
             assert sequence in generated_list
 
 
+class FuseTest2(NamedTuple):
+    input: Sequences
+    expected: Sequences
+
+    def validate(self):
+        output = fuse_by_min_distance(self.input)
+        generated_list = list(output)
+        expected_list = list(self.expected)
+        assert len(expected_list) == len(generated_list)
+        for sequence in expected_list:
+            assert sequence in generated_list
+
 
 tag_tests = [
     TagTest(TagMethod.SpeciesAfterPipe, Sequence("id1", "ATC"), Sequence("id1", "ATC")),
@@ -68,6 +80,10 @@ gap_tests = [
 
 fuse_tests = [
     FuseTest(
+        Sequences([]),
+        Sequences([]),
+    ),
+    FuseTest(
         Sequences([
             Sequence("id1", "AC--", {"species": "X"}),
             Sequence("id2", "ACGT", {"species": "X"}),
@@ -77,6 +93,26 @@ fuse_tests = [
         Sequences([
             Sequence("id2", "ACGT", {"species": "X"}),
             Sequence("id3", "ACGT", {"species": "Y"}),
+        ]),
+    ),
+]
+
+
+fuse_tests2 = [
+    # FuseTest2(
+    #     Sequences([]),
+    #     Sequences([]),
+    # ),
+    FuseTest2(
+        Sequences([
+            Sequence("id1", "ACGT", {"species": "X"}),
+            Sequence("id2", "ACGT", {"species": "X"}),
+            Sequence("id3", "TGCA", {"species": "Y"}),
+            Sequence("id4", "TGGT", {"species": "Y"}),
+        ]),
+        Sequences([
+            Sequence("id1", "ACGT", {"species": "X"}),
+            Sequence("id4", "TGGT", {"species": "Y"}),
         ]),
     ),
 ]
@@ -94,4 +130,8 @@ def test_gap_species(test: GapTest):
 
 @pytest.mark.parametrize("test", fuse_tests)
 def test_fuse_sequences(test: FuseTest):
+    test.validate()
+
+@pytest.mark.parametrize("test", fuse_tests2)
+def test_fuse_sequences_2(test: FuseTest2):
     test.validate()
