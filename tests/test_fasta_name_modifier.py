8df7f8b19d811ee2c65d7b94f45566fe0ad86ee3
Stefanos Patmanidis <stefanpatman91@gmail.com>
2025-04-02 14:21:17 +0300

    Fix fasta name modifier tests

--- tests/test_fasta_name_modifier.py
+++ tests/test_fasta_name_modifier.py
@@ -16,6 +16,7 @@ class FastaNameModifierTest(NamedTuple):
     expected_output: str
     trim: bool
     add: bool
+    replace: bool
     sanitize: bool
     preserve_separators: bool
     trimposition: str
@@ -23,6 +24,8 @@ class FastaNameModifierTest(NamedTuple):
     renameauto: bool
     direc: str | None
     addstring: str | None
+    findstring: str | None
+    replacestring: str | None
     fixaliseparator: bool
     fixseqspaces: bool
     fixseqasterisks: bool
@@ -33,20 +36,23 @@ class FastaNameModifierTest(NamedTuple):
         expected_output = TEST_DATA_DIR / self.expected_output
 
         fasta_name_modifier(
-            str(input_name),
-            str(output_name),
-            self.trim,
-            self.add,
-            self.sanitize,
-            self.preserve_separators,
-            self.trimposition,
-            self.trimmaxchar,
-            self.renameauto,
-            self.direc,
-            self.addstring,
-            self.fixaliseparator,
-            self.fixseqspaces,
-            self.fixseqasterisks,
+            input_name=str(input_name),
+            output_name=str(output_name),
+            trim=self.trim,
+            add=self.add,
+            replace=self.replace,
+            sanitize=self.sanitize,
+            preserve_separators=self.preserve_separators,
+            trimposition=self.trimposition,
+            trimmaxchar=self.trimmaxchar,
+            renameauto=self.renameauto,
+            direc=self.direc,
+            addstring=self.addstring,
+            findstring=self.findstring,
+            replacestring=self.replacestring,
+            fixseqspaces=self.fixseqspaces,
+            fixseqasterisks=self.fixseqasterisks,
+            fixaliseparator=self.fixaliseparator,
         )
 
         assert output_name.exists()
@@ -66,10 +72,11 @@ class FastaNameModifierTest(NamedTuple):
 fasta_name_modifier_tests = [
     FastaNameModifierTest(  # test simple case
         "FastaExample_simple.fas",
-        "simlpe_output.fas",
-        "simlpe_output_expected.fas",
+        "simple_output.fas",
+        "simple_output_expected.fas",
         trim=True,
         add=False,
+        replace=False,
         sanitize=True,
         preserve_separators=False,
         trimposition="end",
@@ -77,6 +84,8 @@ fasta_name_modifier_tests = [
         renameauto=True,
         direc=None,
         addstring=None,
+        findstring=None,
+        replacestring=None,
         fixaliseparator=False,
         fixseqspaces=False,
         fixseqasterisks=False,
@@ -87,6 +96,7 @@ fasta_name_modifier_tests = [
         "complex_output_expected.fas",
         trim=True,
         add=False,
+        replace=False,
         sanitize=True,
         preserve_separators=False,
         trimposition="end",
@@ -94,6 +104,8 @@ fasta_name_modifier_tests = [
         renameauto=True,
         direc=None,
         addstring=None,
+        findstring=None,
+        replacestring=None,
         fixaliseparator=False,
         fixseqspaces=False,
         fixseqasterisks=False,
@@ -104,6 +116,7 @@ fasta_name_modifier_tests = [
         "special_output_expected.fas",
         trim=True,
         add=False,
+        replace=False,
         sanitize=True,
         preserve_separators=False,
         trimposition="end",
@@ -111,16 +124,19 @@ fasta_name_modifier_tests = [
         renameauto=True,
         direc=None,
         addstring=None,
+        findstring=None,
+        replacestring=None,
         fixaliseparator=False,
         fixseqspaces=False,
         fixseqasterisks=False,
     ),
     FastaNameModifierTest(  # test trim and add
         "FastaExample_simple.fas",
-        "simlpe_output_trim_add.fas",
-        "simlpe_output_trim_add_expected.fas",
+        "simple_output_trim_add.fas",
+        "simple_output_trim_add_expected.fas",
         trim=True,
         add=True,
+        replace=False,
         sanitize=True,
         preserve_separators=False,
         trimposition="end",
@@ -128,6 +144,8 @@ fasta_name_modifier_tests = [
         renameauto=True,
         direc="end",
         addstring="end",
+        findstring=None,
+        replacestring=None,
         fixaliseparator=False,
         fixseqspaces=False,
         fixseqasterisks=False,
@@ -138,6 +156,7 @@ fasta_name_modifier_tests = [
         "complex_output_auto_notrim_expected.fas",
         trim=False,
         add=False,
+        replace=False,
         sanitize=False,
         preserve_separators=False,
         trimposition="",
@@ -145,6 +164,8 @@ fasta_name_modifier_tests = [
         renameauto=True,
         direc=None,
         addstring=None,
+        findstring=None,
+        replacestring=None,
         fixaliseparator=False,
         fixseqspaces=False,
         fixseqasterisks=False,
@@ -155,6 +176,7 @@ fasta_name_modifier_tests = [
         "complex_output_auto_trim_san_add_beginning_expected.fas",
         trim=True,
         add=True,
+        replace=False,
         sanitize=True,
         preserve_separators=False,
         trimposition="end",
@@ -162,6 +184,8 @@ fasta_name_modifier_tests = [
         renameauto=True,
         direc="beginning",
         addstring="_",
+        findstring=None,
+        replacestring=None,
         fixaliseparator=False,
         fixseqspaces=False,
         fixseqasterisks=False,
@@ -172,6 +196,7 @@ fasta_name_modifier_tests = [
         "ali_simple_expected.fas",
         trim=False,
         add=False,
+        replace=False,
         sanitize=True,
         preserve_separators=True,
         trimposition="",
@@ -179,9 +204,31 @@ fasta_name_modifier_tests = [
         renameauto=False,
         direc=None,
         addstring=None,
+        findstring=None,
+        replacestring=None,
         fixaliseparator=True,
         fixseqspaces=True,
         fixseqasterisks=True,
+    ),
+    FastaNameModifierTest(  # test replace
+        "FastaExample_simple.fas",
+        "replace_output.fas",
+        "replace_output_expected.fas",
+        trim=False,
+        add=False,
+        replace=True,
+        sanitize=False,
+        preserve_separators=False,
+        trimposition=None,
+        trimmaxchar=None,
+        renameauto=None,
+        direc=None,
+        addstring=None,
+        findstring="MVTIS",
+        replacestring="ISMTV",
+        fixaliseparator=False,
+        fixseqspaces=False,
+        fixseqasterisks=False,
     ),
 ]
 
