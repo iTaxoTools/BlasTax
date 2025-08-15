14d2bd0bcc9937454665042937ad519bce20829a
StefanPatman <stefanpatman91@gmail.com>
2025-08-16 02:57:19 +0300

    Update dashboard and mafft descriptions

--- src/itaxotools/blastax/tasks/mafft/__init__.py
+++ src/itaxotools/blastax/tasks/mafft/__init__.py
@@ -1,13 +1,13 @@
 from itaxotools.blastax.resources import task_pixmaps_large, task_pixmaps_medium
 
-title = "MAFFT"
+title = "MAFFT alignment"
 description = "Multiple sequence alignment"
 
 pixmap = task_pixmaps_large.mafft
 pixmap_medium = task_pixmaps_medium.mafft
 
 long_description = (
-    "Align sequences using MAFFT, a multiple sequence alignment program. "
+    "Align sequences using MAFFTpy, a wrapper for the multiple sequence alignment program. "
     "Only a selected number of strategies and options are available. "
     "\n\n"
     "Input files must be in FASTA format. "
