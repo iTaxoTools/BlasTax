32375d8da336a5d3e06124f23436bab8c488af00
StefanPatman <stefanpatman91@gmail.com>
2026-02-17 19:58:38 +0200

    Add icons for taxo and export tasks

--- src/itaxotools/blastax/tasks/export/__init__.py
+++ src/itaxotools/blastax/tasks/export/__init__.py
@@ -1,9 +1,9 @@
 from itaxotools.blastax.resources import task_pixmaps_large, task_pixmaps_medium
 
-title = "Export BLAST database"
-description = "Convert to FASTA format"
+title = "Database operations"
+description = "Export to FASTA and more"
 
-pixmap = task_pixmaps_large.about
-pixmap_medium = task_pixmaps_medium.about
+pixmap = task_pixmaps_large.export
+pixmap_medium = task_pixmaps_medium.export
 
 long_description = "Extract sequences from a BLAST database and save them in FASTA format. "
--- src/itaxotools/blastax/tasks/taxo/__init__.py
+++ src/itaxotools/blastax/tasks/taxo/__init__.py
@@ -1,10 +1,10 @@
 from itaxotools.blastax.resources import task_pixmaps_large, task_pixmaps_medium
 
-title = "Assign BLAST taxonomy"
+title = "Assign taxonomy"
 description = "Identify taxid and organism"
 
-pixmap = task_pixmaps_large.about
-pixmap_medium = task_pixmaps_medium.about
+pixmap = task_pixmaps_large.taxo
+pixmap_medium = task_pixmaps_medium.taxo
 
 long_description = (
     "Given one or more query files and a BLAST database, search the database for sequence matches "
